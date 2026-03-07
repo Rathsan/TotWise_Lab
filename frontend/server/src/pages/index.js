659d630bbe4b8f1adb6552514ebe7da6595abed9
Sandip Rath <sandiprath@Sandips-MacBook-Air.local>
2026-03-07 17:45:42 +0530

    redirect from html

--- frontend/server/src/pages/index.js
+++ frontend/server/src/pages/index.js
@@ -1,9 +1,17 @@
-export async function getServerSideProps() {
+import fs from 'fs';
+import path from 'path';
+
+export async function getServerSideProps({ res }) {
+  const filePath = path.join(process.cwd(), 'public', 'index.html');
+  const html = fs.readFileSync(filePath, 'utf8');
+
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.write(html);
+  res.end();
+
+  // Next.js requires returning an object, but the response has already been sent.
   return {
-    redirect: {
-      destination: '/index.html',
-      permanent: false
-    }
+    props: {},
   };
 }
 
